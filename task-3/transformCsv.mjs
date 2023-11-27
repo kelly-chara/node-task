80f8fd2ceeb2482cc5ed803e3a91a8bfb7d4e153
kelly-chara <kelly_chara@epam.com>
2023-11-27 04:06:35 -0500

    change to pipeline

--- task-3/transformCsv.mjs
+++ task-3/transformCsv.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import csvtojson from 'csvtojson';
+import {pipeline }from "stream"
 
 const csvFilePath = './csvdirectory/input.csv';
 const txtFilePath = './csvdirectory/output.txt';
@@ -7,23 +8,33 @@ const txtFilePath = './csvdirectory/output.txt';
 const readStream = fs.createReadStream(csvFilePath);
 const writeStream = fs.createWriteStream(txtFilePath);
 
-readStream
-  .pipe(csvtojson())
-  .on('data', (buffer) => {
-    // convert the buffer to a string
-    const jsonString = buffer.toString();
-    // write the objects
-    writeStream.write(jsonString);
-  })
-  .on('error', (error) => {
-    console.error('Error reading CSV file:', error.message);
-  })
-  .on('end', () => {
-    console.log('CSV to JSON conversion complete.');
-    writeStream.end();
-  });
 
-writeStream.on('error', (error) => {
-  console.error('Error writing to the output file:', error.message);
-});
-
+pipeline(
+  readStream
+  .on(
+    "error",(error) => {
+      console.error('Error reading CSV file:', error.message);
+    }
+  ).on("end" , () => {
+    console.log("conversion completed")
+    writeStream.end()
+  }),
+  csvtojson(),
+  writeStream.
+  on("finish", () => 
+  {
+    console.log("finish writing the file")
+  }
+ )
+  .on("error", (error) => { 
+    console.log(`Error writing to file ${error}` )
+  }),
+  
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  }
+);
