84da6d523e42cad5beb7ceda430709856334c992
kelly-chara <kelly_chara@epam.com>
2023-11-30 15:43:42 -0500

    create route to filter by id

--- models/server.js
+++ models/server.js
@@ -1,53 +1,23 @@
 // src/server.ts
 import http from 'http';
+import url from "url";
+
+import { handleAllUsersRoute, handleUsersRoute } from '../routes/users.js';
+import { handleHobbiesRoute } from '../routes/hobbies.js';
+
+
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/users/id' && req.method === "POST") {
-   
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('CREATE User info\n');
-  } 
-  else if (req.url === '/users/id' && req.method === "DELETE") {
+  const parsedUrl = url.parse(req.url, true); // Parse the URL
+  const userId = parsedUrl.pathname.split('/')[2]; // Extract the dynamic part (user ID)
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('DELETE User info\n');
-  }
-else if (req.url === '/users/id' && req.method === "PATCH")
-  {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('UPDATE User info\n');
-  }
-  else if (req.url === '/users/id' && req.method === "GET")
-  {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('GET User info\n');
-  }
-  if (req.url === '/users/id/hobby' && req.method === "POST") {
-   
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('CREATE Hobby\n');
-  } 
-  else if (req.url === '/users/id/hobby' && req.method === "DELETE") {
-
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('DELETE hobby info\n');
-  }
-  else if (req.url === '/users/id/hobby' && req.method === "GET")
-  {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('GET ALL HOBBIES FROM USER\n');
-  }
-  else if (req.url === '/users' && req.method === "GET")
-  {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('GET ALL USERS\n');
-  }
-  else
-  {
+  if (req.url === `/users/${userId}`) {
+    handleUsersRoute(req, res);
+  } else if (req.url === `/users/${userId}/hobby`) {
+    handleHobbiesRoute(req, res);
+  } else if (req.url === '/users' && req.method === "GET") {
+   handleAllUsersRoute(req, res)
+  } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found\n');
   }
